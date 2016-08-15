633d35d6013d96341d62918009c9e5b66b1229b9
michael fahrner <Mike Fahrner>
2016-08-15 19:58:22 -0400

    almost final commit

---
+++ src/sample/Contact.java
@@ -0,0 +1,21 @@
+package sample;
+
+/**
+ * Created by mfahrner on 8/15/16.
+ */
+public class Contact {
+    String name;
+    String phone;
+    String email;
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s, %s", name, phone, email);
+    }
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,51 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+
+public class Controller implements Initializable {
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    public void addContact() {
+        if (!textName.getText().equals("") && !textPhone.getText().equals("") && !textEmail.getText().equals("")) {
+            contacts.add(new Contact(textName.getText(), textPhone.getText(), textEmail.getText()));
+
+        } else if (textName.getText().equals("") && textPhone.getText().equals("") && textEmail.getText().equals("")) {
+
+        }
+    }
+
+    public void removeContact() {
+        Contact item = (Contact) list.getSelectionModel().getSelectedItem();
+        contacts.remove(item);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        list.setItems(contacts);
+    }
+
+    @FXML
+    ListView list;
+
+    @FXML
+    TextField textName;
+
+    @FXML
+    TextField textPhone;
+
+    @FXML
+    TextField textEmail;
 }
